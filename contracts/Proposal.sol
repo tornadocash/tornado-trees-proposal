0159b368f877ebd571d050619c6f7c6d825ff222
Alexey <peretsmobil@gmail.com>
2021-02-11 23:21:01 +0300

    updates

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -8,75 +8,44 @@ import "tornado-trees/contracts/TornadoTrees.sol";
 import "tornado-anonymity-mining/contracts/TornadoProxy.sol";
 import "./interfaces/ITornadoProxyV1.sol";
 import "./interfaces/IMiner.sol";
-import "./interfaces/IDeployer.sol";
 import "./verifiers/BatchTreeUpdateVerifier.sol";
 
 contract Proposal {
   ITornadoTreesV1 public constant tornadoTreesV1 = ITornadoTreesV1(0x43a3bE4Ae954d9869836702AFd10393D3a7Ea417);
   ITornadoProxyV1 public constant tornadoProxyV1 = ITornadoProxyV1(0x905b63Fff465B9fFBF41DeA908CEb12478ec7601);
   IMiner public constant miner = IMiner(0x746Aebc06D2aE31B71ac51429A19D54E797878E9);
-  IDeployer public constant deployer = IDeployer(0xce0042B868300000d44A59004Da54A005ffdcf9f);
-  bytes32 constant deploySalt = 0x0000000000000000000000000000000000000000000000000000000047941987;
-
-  // todo avoid sstore
-  address[4] public miningInstances = [
-    address(0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc),
-    address(0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936),
-    address(0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF),
-    address(0xA160cdAB225685dA1d56aa342Ad8841c3b53f291)
-  ];
-
-  address[1] public allowedInstances = [
-    // todo paste valid addresses except eth
-    // todo use ENS
-    address(0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF)
-  ];
 
   function executeProposal() public {
-    // Disable logging new deposits on old tornado proxy
+    // Disable registering new deposits on old tornado proxy
+    address[4] memory miningInstances = getEthInstances();
     for (uint256 i = 0; i < miningInstances.length; i++) {
       tornadoProxyV1.updateInstance(miningInstances[i], false);
     }
 
-    // Deploy snark verifier for merkle tree updates
-    BatchTreeUpdateVerifier verifier = BatchTreeUpdateVerifier(deployer.deploy(type(BatchTreeUpdateVerifier).creationCode, deploySalt));
-    require(address(verifier) != address(0));
+    BatchTreeUpdateVerifier verifier = new BatchTreeUpdateVerifier();
+
+    uint256 nonce = findNextNonce(address(this), address(verifier), 0);
+    address tornadoProxyAddress = computeAddress(address(this), nonce + 1);
 
     // Deploy new TornadoTrees contract
-    TornadoTrees.SearchParams memory searchParams = TornadoTrees.SearchParams({
-      // todo adjust parameters
-      unprocessedDeposits: 8000,
-      unprocessedWithdrawals: 8000,
-      depositsPerDay: 50,
-      withdrawalsPerDay: 50
-    });
-    TornadoTrees tornadoTrees = new TornadoTrees(
-      address(this),
-      bytes32(0), // todo
-      tornadoTreesV1,
-      IBatchTreeUpdateVerifier(address(verifier)),
-      searchParams
-    );
+    TornadoTrees.SearchParams memory searchParams =
+      TornadoTrees.SearchParams({ // todo adjust parameters
+        unprocessedDeposits: 8000,
+        unprocessedWithdrawals: 8000,
+        depositsPerDay: 50,
+        withdrawalsPerDay: 50
+      });
+    TornadoTrees tornadoTrees =
+      new TornadoTrees(
+        address(this),
+        tornadoProxyAddress,
+        tornadoTreesV1,
+        IBatchTreeUpdateVerifier(address(verifier)),
+        searchParams
+      );
 
     // Deploy new TornadoProxy
-    TornadoProxy.Instance[] memory instances = new TornadoProxy.Instance[](allowedInstances.length + miningInstances.length);
-    for (uint256 i = 0; i < miningInstances.length; i++) {
-      instances[i] = TornadoProxy.Instance(
-        bytes32(uint256(uint160(miningInstances[i]))),
-        TornadoProxy.InstanceState.Mineable
-      );
-    }
-    for (uint256 i = 0; i < allowedInstances.length; i++) {
-      instances[miningInstances.length + i] = TornadoProxy.Instance(
-        bytes32(uint256(uint160(allowedInstances[i]))),
-        TornadoProxy.InstanceState.Enabled
-      );
-    }
-    TornadoProxy proxy = new TornadoProxy(
-      address(tornadoTrees),
-      address(this),
-      instances
-    );
+    TornadoProxy proxy = new TornadoProxy(address(tornadoTrees), address(this), getInstances());
 
     // Update TornadoTrees address on mining contract
     miner.setTornadoTreesContract(address(tornadoTrees));
@@ -86,4 +55,75 @@ contract Proposal {
     require(address(tornadoTrees.tornadoProxy()) == address(proxy), "tornadoProxy deployed to an unexpected address");
   }
 
+  function getEthInstances() public pure returns (address[4] memory) {
+    return [
+      address(0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc),
+      address(0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936),
+      address(0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF),
+      address(0xA160cdAB225685dA1d56aa342Ad8841c3b53f291)
+    ];
+  }
+
+  function getInstances() public pure returns (TornadoProxy.Instance[] memory instances) {
+    // another approach
+    // instances = [
+    //   TornadoProxy.Instance({
+    //     instance: bytes32(0xc041982b4f77cbbd82ef3b9ea748738ac6c281d3f1af198770d29f75ac32d80a),
+    //     state: TornadoProxy.InstanceState.Mineable
+    //   }),
+    //   TornadoProxy.Instance({
+    //     instance: bytes32(0x9e5bc9215eecd103644145a5db4f69d5efaf4885bb5bf968f8db271ec5cd539b),
+    //     state: TornadoProxy.InstanceState.Mineable
+    //   })
+    // ];
+    bytes32[4] memory miningInstances =
+      [
+        bytes32(0xc041982b4f77cbbd82ef3b9ea748738ac6c281d3f1af198770d29f75ac32d80a),
+        bytes32(0x9e5bc9215eecd103644145a5db4f69d5efaf4885bb5bf968f8db271ec5cd539b),
+        bytes32(0x917e42347647689051abc744f502bff342c76ad30c0670b46b305b2f7e1f893d),
+        bytes32(0xddfc726d74f912f49389ef7471e75291969852ce7e5df0509a17bc1e46646985)
+      ];
+
+    bytes32[7] memory allowedInstances =
+      [
+        bytes32(0x95ad5771ba164db3fc73cc74d4436cb6a6babd7a2774911c69d8caae30410982),
+        bytes32(0x109d0334da83a2c3a687972cc806b0eda52ee7a30f3e44e77b39ae2a20248321),
+        bytes32(0xc9395879ffcee571b0dfd062153b27d62a6617e0f272515f2eb6259fe829c3df),
+        bytes32(0xf840ad6cba4dbbab0fa58a13b092556cd53a6eeff716a3c4a41d860a888b6155),
+        bytes32(0xd49809328056ea7b7be70076070bf741ec1a27b86bebafdc484eee88c1834191),
+        bytes32(0x77e2b15eddc494b6da6cee0d797ed30ed3945f2c7de0150f16f0405a12e5665f),
+        bytes32(0x36bab2c045f88613be6004ec1dc0c3937941fcf4d4cb78d814c933bf1cf25baf)
+      ];
+    instances = new TornadoProxy.Instance[](allowedInstances.length + miningInstances.length);
+
+    for (uint256 i = 0; i < miningInstances.length; i++) {
+      instances[i] = TornadoProxy.Instance(miningInstances[i], TornadoProxy.InstanceState.Mineable);
+    }
+    for (uint256 i = 0; i < allowedInstances.length; i++) {
+      instances[miningInstances.length + i] = TornadoProxy.Instance(allowedInstances[i], TornadoProxy.InstanceState.Enabled);
+    }
+  }
+
+  function findNextNonce(
+    address _deployer,
+    address _lastDeployed,
+    uint256 _start
+  ) public pure returns (uint256) {
+    while (computeAddress(_deployer, _start) != _lastDeployed) {
+      _start++;
+    }
+    return _start + 1;
+  }
+
+  function computeAddress(address _origin, uint256 _nonce) public pure returns (address) {
+    bytes memory data;
+    if (_nonce == 0x00) data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(0x80));
+    else if (_nonce <= 0x7f) data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), _origin, bytes1(uint8(_nonce)));
+    else if (_nonce <= 0xff) data = abi.encodePacked(bytes1(0xd7), bytes1(0x94), _origin, bytes1(0x81), uint8(_nonce));
+    else if (_nonce <= 0xffff) data = abi.encodePacked(bytes1(0xd8), bytes1(0x94), _origin, bytes1(0x82), uint16(_nonce));
+    else if (_nonce <= 0xffffff) data = abi.encodePacked(bytes1(0xd9), bytes1(0x94), _origin, bytes1(0x83), uint24(_nonce));
+    else data = abi.encodePacked(bytes1(0xda), bytes1(0x94), _origin, bytes1(0x84), uint32(_nonce));
+    bytes32 hash = keccak256(data);
+    return address(uint160(uint256(hash)));
+  }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 /* global task, ethers */
 require('@nomiclabs/hardhat-waffle')
+const ens = require('eth-ens-namehash')
 require('dotenv').config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -9,6 +10,32 @@ task('accounts', 'Prints the list of accounts', async () => {
   for (const account of accounts) {
     console.log(account.address)
   }
+})
+
+task('namehashes', 'Prints the list of tornado instances and corresponding ens namehashes', () => {
+  const mineable = [
+    'eth-01.tornadocash.eth',
+    'eth-1.tornadocash.eth',
+    'eth-10.tornadocash.eth',
+    'eth-100.tornadocash.eth',
+  ]
+  const allowed = [
+    'dai-100.tornadocash.eth',
+    'dai-1000.tornadocash.eth',
+    'cdai-5000.tornadocash.eth',
+    'cdai-50000.tornadocash.eth',
+    'usdc-100.tornadocash.eth',
+    'usdc-1000.tornadocash.eth',
+    'usdt-100.tornadocash.eth',
+  ]
+  console.log('Allowed instances:')
+  allowed.forEach((name) => {
+    console.log(`${name} - ${ens.hash(name)}`)
+  })
+  console.log('Allowed and mineable instances:')
+  mineable.forEach((name) => {
+    console.log(`${name} - ${ens.hash(name)}`)
+  })
 })
 
 // You need to export an object to set up your config
