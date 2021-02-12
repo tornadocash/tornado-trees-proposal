0c483698ff7663ef4f67ca23111bd3dae6d3943e
poma <semenov.roma@gmail.com>
2021-02-12 11:52:43 +0300

    fixes

--- contracts/mocks/ITornadoTrees.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
-pragma experimental ABIEncoderV2;
-
-import "tornado-trees/contracts/TornadoTrees.sol";
-
-abstract contract TornadoTreesMock is TornadoTrees {}
