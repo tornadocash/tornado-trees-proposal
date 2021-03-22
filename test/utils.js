a57a42c0e06a32107aec31d4fff5ecc4a552a510
poma <semenov.roma@gmail.com>
2021-03-22 23:40:09 +0300

    update instances

--- test/utils.js
+++ test/utils.js
@@ -6,20 +6,28 @@ const { poseidonHash } = require('tornado-trees/src/utils')
 const abi = new ethers.utils.AbiCoder()
 
 const ethInstances = [
-  '0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc',
-  '0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936',
-  '0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF',
-  '0xA160cdAB225685dA1d56aa342Ad8841c3b53f291',
+  '0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc', // eth-01.tornadocash.eth
+  '0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936', // eth-1.tornadocash.eth
+  '0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF', // eth-10.tornadocash.eth
+  '0xA160cdAB225685dA1d56aa342Ad8841c3b53f291', // eth-100.tornadocash.eth
 ]
 
 const erc20Instances = [
-  '0xD4B88Df4D29F5CedD6857912842cff3b20C8Cfa3',
-  '0xFD8610d20aA15b7B2E3Be39B396a1bC3516c7144',
-  '0x22aaA7720ddd5388A3c0A3333430953C68f1849b',
-  '0xd96f2B1c14Db8458374d9Aca76E26c3D18364307',
-  '0x4736dCf1b7A3d580672CcE6E7c65cd5cc9cFBa9D',
-  '0x169AD27A470D064DEDE56a2D3ff727986b15D52B',
-  '0x0836222F2B2B24A3F36f98668Ed8F0B38D1a872f',
+  '0x07687e702b410Fa43f4cB4Af7FA097918ffD2730', // dai-10000.tornadocash.eth
+  '0x23773E65ed146A459791799d01336DB287f25334', // dai-100000.tornadocash.eth
+  '0x03893a7c7463AE47D46bc7f091665f1893656003', // cdai-50000.tornadocash.eth
+  '0x2717c5e28cf931547B621a5dddb772Ab6A35B701', // cdai-500000.tornadocash.eth
+  '0xD21be7248e0197Ee08E0c20D4a96DEBdaC3D20Af', // cdai-5000000.tornadocash.eth
+  '0x178169B423a011fff22B9e3F3abeA13414dDD0F1', // wbtc-01.tornadocash.eth
+  '0x610B717796ad172B316836AC95a2ffad065CeaB4', // wbtc-1.tornadocash.eth
+  '0xbB93e510BbCD0B7beb5A853875f9eC60275CF498', // wbtc-10.tornadocash.eth
+  '0xD4B88Df4D29F5CedD6857912842cff3b20C8Cfa3', // dai-100.tornadocash.eth
+  '0xFD8610d20aA15b7B2E3Be39B396a1bC3516c7144', // dai-1000.tornadocash.eth
+  '0x22aaA7720ddd5388A3c0A3333430953C68f1849b', // cdai-5000.tornadocash.eth
+  '0xd96f2B1c14Db8458374d9Aca76E26c3D18364307', // usdc-100.tornadocash.eth
+  '0x4736dCf1b7A3d580672CcE6E7c65cd5cc9cFBa9D', // usdc-1000.tornadocash.eth
+  '0x169AD27A470D064DEDE56a2D3ff727986b15D52B', // usdt-100.tornadocash.eth
+  '0x0836222F2B2B24A3F36f98668Ed8F0B38D1a872f', // usdt-1000.tornadocash.eth
 ]
 
 async function setTime(timestamp) {
